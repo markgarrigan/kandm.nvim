bb494d0a743e58e2f3b0d625723ffc7b33005668
Mark Garrigan <mrkgrrgn@gmail.com>
2023-11-17 09:02:40 -0600

    first commit

---
+++ mappings.lua
@@ -0,0 +1,21 @@
+local M = {}
+
+-- In order to disable a default keymap, use
+M.disabled = {}
+
+-- Your custom mappings
+M.abc = {
+  n = {
+    ['<leader>wc'] = {'<cmd>close<CR>', '[W]indow [C]lose.'},
+  },
+
+  i = {
+    ['<C-s>'] = {'<ESC>:w<CR>l ', 'Save current buffer in insert mode.'}
+  },
+
+  v = {
+    ['<C-s>'] = {'<ESC>:w<CR> ', 'Save current buffer in visual mode'}
+  },
+}
+
+return M
